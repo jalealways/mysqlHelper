876b700e2b1a682fa13102bb86d737adc2447caf
jale <jale_me@163.com>
2017-03-26 22:24:50 +0800

    first

---
+++ mysqlHelper.py
@@ -0,0 +1,74 @@
+#encoding=utf8
+import MySQLdb
+
+class MysqlHelper():
+    def __init__(self,host,port,db,user,passwd,charset='utf8'):
+        self.host=host
+        self.port=port
+        self.db=db
+        self.user=user
+        self.passwd=passwd
+        self.charset=charset
+
+    def connect(self):
+        self.conn=MySQLdb.connect(host=self.host,port=self.port,db=self.db,user=self.user,passwd=self.passwd,charset=self.charset)
+        self.cursor=self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+    def get_one(self,sql,params=()):
+        result=None
+        try:
+            self.connect()
+            self.cursor.execute(sql, params)
+            result = self.cursor.fetchone()
+            self.close()
+        except Exception, e:
+            print e.message
+        return result
+
+    def get_all(self,sql,params=()):
+        list=()
+        try:
+            self.connect()
+            self.cursor.execute(sql,params)
+            list=self.cursor.fetchall()
+            self.close()
+        except Exception,e:
+            print e.message
+        return list
+
+    def insert(self,sql,params=()):
+        return self.__edit(sql,params)
+
+    def update(self, sql, params=()):
+        return self.__edit(sql, params)
+
+    def delete(self, sql, params=()):
+        return self.__edit(sql, params)
+
+    def __edit(self,sql,params):
+        count=0
+        try:
+            self.connect()
+            count=self.cursor.execute(sql,params)
+            self.conn.commit()
+            self.close()
+        except Exception,e:
+            print e.message
+        return count
+
+if __name__ == '__main__':
+        num = 0
+        for i in range(5):
+            num += 1
+	    sql='insert into student(name,gender) values(%s,%s)'
+	    params=['jack' + str(num), True]
+	    mysqlHelper=MysqlHelper('localhost',3306,'test1','root','mysql')
+	    count=mysqlHelper.insert(sql,params)
+	    if count==1:
+    	        print num
+	    else:
+    	        print 'error'
